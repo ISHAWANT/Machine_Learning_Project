00c06a49eab97d9168383b7341835d2a999f12b6
Ishawant <ishawantkumar@gmail.com>
2022-09-12 20:14:42 +0530

    change

--- housing/entity/config_entity.py
+++ housing/entity/config_entity.py
@@ -4,3 +4,19 @@ from collections import namedtuple
 DataIngestionConfig=namedtuple("DataIngestionConfig",["dataset_download_url","tgz_download_dir","raw_data_dir",
 "ingested_train_dir","ingested_test_dir"])
 
+DataValidationConfig = namedtuple("DataValidationConfig",["schema_file_path"])
+
+DataTransformatConfig = namedtuple("DataTransformationConfig",["add_bedroom_per_room",
+                                                                "transformed_train_dir",
+                                                                "transformed_test_dir",
+                                                                "preprocessed_object_file_path"])
+
+
+ModelTraierConfig =namedtuple("ModelTrainerConfig",["trained_model_file_path","base_accuracy"])
+
+
+ModelEvaluationConfig = namedtuple("ModelEvaluationConfig",["model_evaluation_file_path","time_stamp"])
+
+
+ModelPusherConfig = namedtuple("ModelPusherConfig",["export_dir_path"])
+
