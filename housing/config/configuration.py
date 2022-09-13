57be3d396f2bee7be7992a75762bbb306687de73
Ishawant <ishawantkumar@gmail.com>
2022-09-13 14:30:58 +0530

    l

---
+++ housing/config/configuration.py
@@ -0,0 +1,21 @@
+
+
+class Configuration:
+    def __init__(self) -> None:
+        pass
+
+    def get_data_ingestion_config(self):
+        pass
+    def get_data_transformation_config(self):
+        pass
+    def model_trainer_config(self):
+        pass
+
+    def get_model_evaluation_config(self):
+        pass
+
+    def get_model_pusher_config(self):
+        pass
+    def get_training_pipeline_config(self):
+        pass
+    
