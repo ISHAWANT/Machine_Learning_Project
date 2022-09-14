fdfab73fe467a1f92c72b58919ae85f3d9269b6f
Ishawant <ishawantkumar@gmail.com>
2022-09-14 16:36:17 +0530

    25th june

--- housing/constant/__init__.py
+++ housing/constant/__init__.py
@@ -22,10 +22,11 @@ TRAINING_PIPELINE_NAME_KEY = "pipeline_name"
 # Data ingestion related variable
 
 DATA_INGESTION_CONFIG_KEY = "data_ingestion_config"
-DATA_INGESTION_ARTIFACT_DIR = "data ingestion"
-DATA_INGESTION__DOWNLOAD_URL_KEY = "dataset download_url"
-DATA_INGESTION__RAW_DATA_DIR_KEY = "raw data dir"
-DATA_INGESTION__TGZ_DOWNLOAD_DIR_KEY = "tgz_download_dir"
-DATA_INGESTION__DIR_NAME_KEY = "ingested_dir"
+DATA_INGESTION_ARTIFACT_DIR = "data_ingestion"
+DATA_INGESTION_DOWNLOAD_URL_KEY = "dataset_download_url"
+DATA_INGESTION_RAW_DATA_DIR_KEY = "raw_data_dir"
+DATA_INGESTION_TGZ_DOWNLOAD_DIR_KEY = "tgz_download_dir"
+DATA_INGESTION_DIR_NAME_KEY = "ingested_dir"
 DATA_INGESTION_TRAIN_DIR_KEY = "ingested_train_dir"
-DATA_INGESTION__TEST_DIR_KEY = "ingested_train_dir"
+DATA_INGESTION_TEST_DIR_KEY = "ingested_train_dir"
+
